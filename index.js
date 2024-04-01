49192b9e309b262b534b54f99826200ad5f1a82f
Peninah83 <160120224+Peninah83@users.noreply.github.com>
2024-04-01 17:26:07 +0300

    added apiURL

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+function refreshWeather(response) {
+    console.log(response.data);
+}
+
+
 function searchCity(city) {
 let apiKey = "3c00030c6541t3abb3aocab358160fcf";
-let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&key={key}`;
-console.log(apiURL);
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
+axios.get(apiURL).then(refreshWeather);
 }
 
 
