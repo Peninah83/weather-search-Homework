8a2191234fff00551952e6d080c03595aa817d25
Peninah83 <160120224+Peninah83@users.noreply.github.com>
2024-04-01 16:34:00 +0300

    added API

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+function searchCity(city) {
+let apiKey = "3c00030c6541t3abb3aocab358160fcf";
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&key={key}`;
+console.log(apiURL);
+}
+
+
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input")
+    let searchInput = document.querySelector("#search-form-input");
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = searchInput.value;
-
+searchCity(searchInput.value);
 
 
 
