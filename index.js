d85acc563a69a34a221e2bbee364e7b4291a7746
Peninah83 <160120224+Peninah83@users.noreply.github.com>
2024-04-02 01:10:06 +0300

    added weather data in js

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
-    
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+
+
+    console.log(response.data);
+
+
  cityElement.innerHTML = response.data.city;
+ descriptionElement.innerHTML = response.data.condition.description
+ humidityElement.innerHTML = `${response.data.temperature.humidity}%;
 temperatureElement.innerHTML = Math.round(temperature);
 }
 
 
 function searchCity(city) {
 let apiKey = "3c00030c6541t3abb3aocab358160fcf";
-let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiURL).then(refreshWeather);
 }
 
@@ -22,6 +30,9 @@ function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
+
+
+  
 
     
 searchCity(searchInput.value);
