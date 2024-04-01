03cb7b06eea6ac251d5443b9eb7f81f171defbc2
Peninah83 <160120224+Peninah83@users.noreply.github.com>
2024-04-01 23:43:09 +0300

    added search functionality using API

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 function refreshWeather(response) {
-    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    
+ cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature);
 }
 
 
 function searchCity(city) {
 let apiKey = "3c00030c6541t3abb3aocab358160fcf";
-let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 axios.get(apiURL).then(refreshWeather);
 }
 
@@ -16,16 +21,15 @@ axios.get(apiURL).then(refreshWeather);
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
-searchCity(searchInput.value);
+    let cityElement = document.querySelector("#city");
 
+    
+searchCity(searchInput.value);
 
 
 }
 
-
-
-
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Kericho");
