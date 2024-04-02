ec9b9246fceaad4952e39bc07e9fb7ca48e555f1
Peninah83 <160120224+Peninah83@users.noreply.github.com>
2024-04-02 13:35:35 +0300

    added  more weather data on js

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ function refreshWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
+    console.log(response.data);
 
+ icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
  cityElement.innerHTML = response.data.city;
- timeElement.innerHTML = formatDate(date);
+ timeElement.innerHTML =formatDate(date);
  descriptionElement.innerHTML = response.data.condition.description
  humidityElement.innerHTML = `${response.data.temperature.humidity}% `;
  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
