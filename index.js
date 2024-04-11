0303c6c49c7c25f2fdb5e258f83a808527e02c09
Peninah83 <160120224+Peninah83@users.noreply.github.com>
2024-04-11 15:09:02 +0300

    updated the html,css and js

--- index.js
+++ index.js
@@ -1,84 +1,94 @@
-function refreshWeather(response) {
+function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
+    let temperature = Math.round(response.data.temperature.current);
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
-    let timeElement = document.querySelector("#time");
-    let date = new Date(response.data.time * 1000);
     let iconElement = document.querySelector("#icon");
-
-    console.log(response.data);
-
- icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
- cityElement.innerHTML = response.data.city;
- timeElement.innerHTML =formatDate(date);
- descriptionElement.innerHTML = response.data.condition.description
- humidityElement.innerHTML = `${response.data.temperature.humidity}% `;
- windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
- temperatureElement.innerHTML = Math.round(temperature);
- 
-
-}
+    
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 
 
-
-function formatDate(date) {
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    temperatureElement.innerHTML = temperature;
+  }
+  
+  function search(event) {
+    event.preventDefault();
+    let searchInputElement = document.querySelector("#search-form-input");
+    let city = searchInputElement.value;
+  
+    let apiKey = "c2fa2f0a85c4ebtf3304fbofa4a162d0";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  
+    axios.get(apiUrl).then(displayTemperature);
+  }
+  
+  function formatDate(date) {
     let minutes = date.getMinutes();
     let hours = date.getHours();
-    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",
-];
-let day = days[date.getDay()];
-
-if (minutes < 10) {
-    minutes = `0${minutes}`;
-}
-
-return `${day} ${hours} : ${minutes}`;
-
-
-}
-
-function searchCity(city) {
-let apiKey = "3c00030c6541t3abb3aocab358160fcf";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(refreshWeather);
-}
-
-
-
-
-
-function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-
-
+    let day = date.getDay();
   
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+  
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+  
+    let formattedDay = days[day];
+    return `${formattedDay} ${hours}:${minutes}`;
+  }
 
-    
-searchCity(searchInput.value);
+  function displayForecast()  {
+    let forecastElement = document.querySelector("#forecast");
 
+    let days = ['Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+    let forecastHtml = "";
 
-}
+    days.forEach(function(day) {
+      forecastHtml = 
+      forecastHtml + 
+      `
+      <div class="weather-forecast-day">
+                  <div class="weather-forecast-date">
+                  ${day}
+                  </div>
+                  <div class = "weather-forecast-icon">
+                  🌤️
+                  </div>
+                  <div class="weather-forecast-temperature">
+                  
+                  <span class="maximum-temperature">18°</span> 
+                  <span class="minimum-temperature">12°</span>
+                  </div>
+      </div>
+      `;
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("Kericho");
+    forecastElement.innerHTML = forecastHtml;
+  }
+  
+  let searchForm = document.querySelector("#search-form");
+  searchForm.addEventListener("submit", search);
+  
+  let currentDateELement = document.querySelector("#current-date");
+  let currentDate = new Date();
+  
+  currentDateELement.innerHTML = formatDate(currentDate);
+displayForecast();
